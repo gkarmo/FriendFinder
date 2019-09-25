55b253922cb5ae7e666503555df173544d14aff0
gkarmo <karmo.gayah@gmail.com>
2019-09-25 03:14:59 -0700

    modal not working/getting heroku ready

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,15 +7,38 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        console.log(req.body.scores);
 
         var newFriend = req.body;
 
         //Convert each user's results into a simple array of numbers
         for (var i = 0; i < newFriend.scores.length; i++) {
-            newFriend.scores[i] = parseInt(newUser.scores[i]);
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
         }
 
         var totalDifference;
+
+        var newFriendIndex = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < friendsData.length; i++) {
+             totalDifference = 0;
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                var difference = Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if (totalDifference < minimumDifference) {
+                newFriendIndex = i;
+                minimumDifference = totalDifference;
+            }
+        }
+
+        friendsData.push(newFriend);
+
+ 
+        res.json(friendsData[newFriendIndex]);
+
     });
 
 };
